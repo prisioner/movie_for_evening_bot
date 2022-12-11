f45f123864b7225a055cde3d961a05fe4452814e
Aleksandr Klimenkov <prisioner@mail.ru>
2022-12-11 21:50:39 +0300

    add bot secret confirmation

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,5 +1,10 @@
 class CallbacksController < ApplicationController
   def telegram
+    unless telegram_header == ENV.fetch("TELEGRAM_BOT_SECRET")
+      head :forbidden
+      return
+    end
+
     unless params.dig(:callback, :message).present?
       head :ok
       return
@@ -8,5 +13,9 @@ class CallbacksController < ApplicationController
     TgBot::Operations::ReactToMessage.call(params[:callback])
 
     head :ok
+  end
+
+  def telegram_header
+    request.headers['HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN']
   end
 end
